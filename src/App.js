58be2d49a523d9e46c6a3f106036adb2b93e344a
krittiyaclark <krittiyaclark@gmail.com>
2019-03-18 17:52:39 -0400

    Styling

--- src/App.js
+++ src/App.js
@@ -26,16 +26,15 @@ class App extends Component {
             <div className="row">
               <div className="col-md-4 offset-md-4">
                 <form>
-                  <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                  <div className="form-group">
+                    <label htmlFor="exampleInputEmail1" className="inputLabel">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Username / email"></input>
                   </div>
-                  <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+                  <div className="form-group">
+                    <label htmlFor="exampleInputPassword1" className="inputLabel">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"></input>
                   </div>
-                  <button type="submit" class="btn btn-primary">Submit</button>
+                  <button type="submit" className="btn btn-primary submit">Submit</button>
                 </form>
 
               </div>
@@ -47,7 +46,19 @@ class App extends Component {
           <div className="container-fluid">
             <div className="row">
               <div className="col-md-4 offset-md-4">
-                .col-md-4 .offset-md-4
+                <div className="col">
+                    <span><a href="https://app.zeplin.io/forgot-password">Forgot Password?</a></span>
+                    <span>Don't have an account? <a href="https://app.zeplin.io/register">Get started</a></span>
+                </div>
+                <div  className="col" id="downloadApp">
+                    <span>
+                        Looking for the
+                        <span className="mac">Mac</span><span className="windows">Windows</span> app?
+                        <a href="https://app.zeplin.io/downloads" className="downloadsLink">Download here</a>
+                        <a href="https://zpl.io/download-mac" id="macDownloadLink">Download here</a>
+                        <a href="https://zpl.io/download-windows-64" id="windowsDownloadLink">Download here</a>
+                    </span>
+                </div>
               </div>
             </div>
           </div>
